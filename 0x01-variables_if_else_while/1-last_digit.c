d0ed479ae4fcc1439da29e588438e42f32b54ec4
Stephenisaac1989 <ogahstephen1989@gmail.com>
2022-12-09 17:18:53 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,14 @@
  *
  *  * main - Entry point
  *
- *   
+ *   *
  *
- *   
+ *    *
  *
- *   Return: Always 0 (Sucess)
+ *     * Return: Always 0 (Sucess)
  *
- */
+ *      */
+
 int main(void)
 
 {
@@ -27,9 +28,9 @@ int main(void)
 
 
 
-		srand(time(0));
+			srand(time(0));
 
-		n = rand() - RAND_MAX / 2;
+				n = rand() - RAND_MAX / 2;
 
 					if (n % 10 != 0 && n % 10 < 6)
 
@@ -39,26 +40,22 @@ int main(void)
 
 											}
 
-					else if (n % 10 > 5)
+						else if (n % 10 > 5)
 
-							{
+								{
 
 											printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 
 												}
 
-					else
+							else
 
-							{
+									{
 
 												printf("Last digit of %d is %d and is 0\n", n, n % 10);
 
 													}
 
-					return (0);
+								return (0);
 
 }
-
-
-
-
