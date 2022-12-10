471cd68c1b4477ea6c107fdd6a0fb48504ea54c7
Stephenisaac1989 <ogahstephen1989@gmail.com>
2022-12-09 22:41:59 -0800

    task1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,17 @@
  *   *
  *    *
  *     * Return: Always 0 (Sucess)
- *      */
+ */
 
 int main(void)
 {
 		char c;
 
 			for (c = 'a' ; c <= 'z' ; c++)
-					{
-								putchar(c);
-									}
+					
+				putchar(c);
+								
 				putchar('\n');
-					return (0);
+				return (0);
 }
 
