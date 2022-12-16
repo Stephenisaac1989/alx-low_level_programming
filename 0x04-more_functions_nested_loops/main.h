6cf8c6331352711be7ea6bdac264bac259b715bd
Stephenisaac1989 <ogahstephen1989@gmail.com>
2022-12-15 19:53:39 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,8 +16,8 @@ void print_alphabet(void);
 /* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 
-/* void print_most_numbers(void);*/
-void print_most_numbers(void);
+/* void more_numbers(void);*/
+void more_numbers(void);
 
 
 /* 3-islower.c */
